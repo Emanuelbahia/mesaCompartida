8ed67000d261b638eff66885697af335b0f6ba97
Emanuelbahia <emanuelazcona@hotmail.com>
2023-08-31 16:04:46 -0300

    main

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { AuthProvider } from './components/context/authContext.jsx';
 import App from './App.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
@@ -11,9 +10,7 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <AuthProvider >
         <App />
-      </AuthProvider>  
     </BrowserRouter>
   </React.StrictMode>,
 )
